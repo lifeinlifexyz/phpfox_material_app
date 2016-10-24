55802b1c29a7fe3a784f39f3426998e9ee76add7
bolot <bolotkalil@gmail.com>
2016-10-24 23:05:41 +0600

    I have added missed file;

---
+++ PF.Base/module/cmmaterial/template/default/controller/index-visitor.html.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * [PHPFOX_HEADER]
+ * 
+ * @copyright		[YouNetCo]
+ * @author  		YouNetCo
+ * @package 		Ynclean
+ * @version 		4.01
+ */
+ 
+defined('PHPFOX') or exit('NO DICE!'); 
+
+?>
+
+<div class="image_load parent-block" data-apply="row_image" data-src="{$image.image}"></div>
